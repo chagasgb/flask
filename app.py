57b809fdeea9b49794a1952db527be0caa650f20
Gabriel Chagas <biel.silva.chagas@gmail.com>
2023-09-26 11:17:03 -0300

    opa

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine 
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config['MONGODB_SETTINGS'] = {
+    "db": "users",
+    "host": "mongodb",
+    "port": 27017,
+    "username": "admin",
+    "password": "admin"
+}
+
+db = MongoEngine(app)
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
+
 class Users(Resource):
     def get(self):
-        return {"message": "user 1"}
+        return jsonify(UserModel.objects())
+        #return {"message": "user 1"}
 
 class User(Resource):
     def post(self):
