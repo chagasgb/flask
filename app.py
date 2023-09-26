dff6a1310ea0c88b863b121b47c29c2254b9b4fd
Gabriel Chagas <biel.silva.chagas@gmail.com>
2023-09-26 15:57:49 -0300

    ds

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
-from flask_mongoengine import MongoEngine 
+from flask_restful import Resource, Api, reqparse
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,6 +15,10 @@ app.config['MONGODB_SETTINGS'] = {
 
 db = MongoEngine(app)
 
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str, help='Nome do objeto')
+
 class UserModel(db.Document):
     cpf = db.StringField(required=True, unique=True)
     first_name = db.StringField(required=True)
@@ -24,15 +28,26 @@ class UserModel(db.Document):
 
 class Users(Resource):
     def get(self):
-        return jsonify(UserModel.objects())
-        #return {"message": "user 1"}
+        args = parser.parse_args()
+        name = args['name']     
+        return {'nome': name} 
+    
+    #   return {"message": "user 1"}
 
-class User(Resource):
-    def post(self):
-        return {"message": "teste"}
+#class User(Resource):
+#    def post(self):
+#        #data = _user_parser.parse_args()
+#        return {"message": "CPF"}
+#        #return data
+#    def post(self, name):
+#        args = parser.parse_args()
+#        name = args['name']
+#        
+#        return {'nome': name}
+
 
 api.add_resource(Users, "/users")
-api.add_resource(User, "/user", "/user/<string:cpf>")
+#api.add_resource(User, "/user", "/user/<string:cpf>")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
