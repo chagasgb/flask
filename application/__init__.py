e17ca6e64a6ae7abb1996dfc12a08da8b92d01df
Chagas <biel.silva.chagas@gmail.com>
2023-12-19 22:05:17 -0300

    OPA

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api, Resource
 from .db import init_db
 from .controllers.transacao_controller import TransacaoController
 from .controllers.ativos_controller import AtivosController
 from .services.data_ingest import IngestData
-
 
 class ConnectionResource(Resource):
     def get(self):
@@ -16,13 +16,14 @@ def create_app(config):
     app.config.from_object(config)
     init_db(app)
 
+    # Configuração básica do CORS (permitindo todas as origens)
+    CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET,POST"], headers=['Content-Type'])
+
     api.add_resource(TransacaoController, '/transacao')
     api.add_resource(AtivosController, '/ativos')
     
     # Adicione o novo recurso à rota raiz ("/")
-    api.add_resource(ConnectionResource, '/'), 200
+    api.add_resource(ConnectionResource, '/')
     api.add_resource(IngestData, '/ingest')
-
-    #api.add_resource(IngestData, '/oie')
 
     return app
